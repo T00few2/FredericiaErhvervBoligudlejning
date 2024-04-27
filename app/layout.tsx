97f0a35533a03f6ce9bf57b19a7151858a26578f
T00few2 <christian.kjar@gmail.com>
2024-04-27 22:51:44 +0200

    chakra

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+// ./app/layout.js
+
+// app/layout.tsx
+
+
+export const metadata = {
+  title: 'Kjærs Boligudlejning',
+  description: 'Boligudlejning',
+}
+
+
+// app/layout.tsx
+import { fonts } from './fonts'
+import { Providers } from './providers'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode,
+}) {
+  return (
+    <html lang='en' className={fonts.rubik.variable}>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  )
+}
+
