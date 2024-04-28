027733c35e8eb0363ee47d6169b3598729ddc65a
T00few2 <christian.kjar@gmail.com>
2024-04-28 12:05:04 +0200

    reval

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,38 @@
 // ./app/page.tsx
 
-
 import client from '../services/apollo-client';
-
-import Apartment from '../components/apartment';
+import {GetLejligheder} from '../queries/get-lejligheder';
 
 import { Image } from '@chakra-ui/react';
 
 export const revalidate = 10;
 
-
-export default async function Home() {
-  
-
-  return (
-    <div>
-      <h1>Kjær Boligudlejning</h1>
-      <Apartment />
-    </div>
-  );
+async function getData() {
+  const {data} = await client.query({
+    query: GetLejligheder,
+  });
+  return data.Lejligheder.items;
 }
+
+export default async function Page() {
+    const lejligheder = await getData();
+  
+    return (
+      <div>
+        <ul>
+          {lejligheder.map((lejlighed) => (
+            <li key={lejlighed._id}>
+  
+              {/* Add links to the artice title and use the article slug to open the new page */}
+              {lejlighed.navn}
+              {lejlighed.beskrivelse}
+              {lejlighed.billeder.map((billede,index) => (
+                <Image src = {billede.url}  alt={`Image ${index + 1}`} key = {index}></Image>
+                
+              ))}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
