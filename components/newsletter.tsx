4f6713a7fb47da0396cf6eda56e8f1bac493739d
T00few2 <christian.kjar@gmail.com>
2024-05-03 22:53:16 +0200

    jkh

--- components/newsletter.tsx
+++ components/newsletter.tsx
@@ -1,15 +1,27 @@
 'use client'
 import { FormEvent, useState } from "react";
 import axios from "axios";
-import { Button, Input, Wrap, VStack, Tooltip, Modal, ModalHeader, ModalBody,ModalOverlay, ModalContent, ModalCloseButton, Icon, Text, Flex } from "@chakra-ui/react";
+import { Button, Input, Wrap, VStack, Tooltip, Modal, ModalHeader, ModalBody,ModalOverlay, ModalContent, ModalCloseButton, Icon, Text, Flex, SimpleGrid, Textarea } from "@chakra-ui/react";
 import { useDisclosure } from '@chakra-ui/react';
 import { FaRegNewspaper } from "react-icons/fa";
+import {
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+    GridItem,
+  } from '@chakra-ui/react'
 
 const Newsletter = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure();
+  const [input, setInput] = useState('')
+  const handleInputChange = (e) => setInput(e.target.value)
+
+  const isError = input === ''
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const [email, setEmail] = useState<string>("");
   const [first_name, setFirstName] = useState<string>('');
   const [last_name, setLastName] = useState<string>('');
+  const [wish, setWish] = useState<string>('');
   const [phone_number, setPhoneNumber] = useState<string>('');
   const [status, setStatus] = useState<
     "success" | "error" | "loading" | "idle"
@@ -26,7 +38,9 @@ const Newsletter = () => {
         email: email,
         fname: first_name,
         lname: last_name,
-        phone: phone_number, });
+        phone: phone_number,
+        wish: wish ,
+    });
       setStatus("success");
       setStatusCode(response.status);
       setEmail("");
@@ -43,10 +57,10 @@ const Newsletter = () => {
 
   return (
     <div>
-        <Flex  align="center" flexDirection="column"> {/* Flex container to align items vertically */}
+        <Flex align="center" flexDirection="column"> {/* Flex container to align items vertically */}
         <Flex align="center"> {/* Flex container to align items horizontally */}
             <Icon onClick = {onOpen} as={FaRegNewspaper} />
-            <Text onClick = {onOpen} color='black' whiteSpace="pre-line" ml={2}>Nyhedsbrev</Text> {/* Text */}
+            <Text onClick = {onOpen} color='black' whiteSpace="pre-line" ml={2}>Mailliste</Text> {/* Text */}
         </Flex>
         </Flex>
   
@@ -60,69 +74,89 @@ const Newsletter = () => {
               backdropBlur='2px'
               />
               <ModalContent backgroundColor={'GhostWhite'} justifyContent={'center'}>
-              <ModalHeader fontSize={'3xl'} fontWeight={'700'}>Nyhedsbrev</ModalHeader>
+              <ModalHeader fontSize={'3xl'} fontWeight={'700'}>Mailliste</ModalHeader>
               <ModalCloseButton />
               <ModalBody>
-                <Text>Tilmeld dig vores mail liste og få besked når der kommer ledige ledligheder</Text>
-              <Wrap justify={'left'} flexWrap={'wrap'} mt={4}>
-      <form
+                <Text mb={4}>Tilmeld dig vores mail liste og få besked når der kommer ledige ledligheder</Text>
+             
+              <form onSubmit={handleSubscribe}>
+                <SimpleGrid columns={2} spacing={4}>
+                    <GridItem>
+                        <FormLabel>Fornavn</FormLabel>
+                        <Input
+                            type="text"
+                            placeholder="Fornavn"
+                            value={first_name}
+                            onChange={(e) => setFirstName(e.target.value)}
+                            disabled={status == "loading"}
+                            size={'md'}
+                        />
+                        </GridItem>
+                    <GridItem>
+                    <FormLabel>Efternavn</FormLabel>
+                    <Input
+                        type="text"
+                        placeholder="Efternavn"
+                        value={last_name}
+                        onChange={(e) => setLastName(e.target.value)}
+                        disabled={status == "loading"}
+                        size={'md'}
+                    />
+                    </GridItem>
+                    <GridItem>
+                    <FormLabel>Telefon</FormLabel>
+                    <Input
+                        type="text"
+                        placeholder="Telefon nummer"
+                        value={phone_number}
+                        onChange={(e) => setPhoneNumber(e.target.value)}
+                        disabled={status == "loading"}
+                        size={'md'}
+                    />
+                    </GridItem>
+                    <FormControl isInvalid={isError} >          
+                    <FormLabel>Email</FormLabel>
+                    <Input 
+                        type='email'
+                        placeholder="E-mail"
+                        onChange={(e) => {
+                            handleInputChange(e);
+                            setEmail(e.target.value);
+                        }}
+                        value={email}
+                        disabled={status == "loading"}
+                        size={'md'} />
+                            {!isError ? (
+                          <></>
+                            ) : (
+                                <FormErrorMessage>Email er nødvendig.</FormErrorMessage>
+                            )}
+                    </FormControl>
+                    </SimpleGrid>
+                    <Textarea
+                        placeholder="Fortæl lidt om hvad du leder efter"
+                        value={wish}
+                        onChange={(e) => setWish(e.target.value)}
+                        disabled={status == "loading"}
+                        size={'lg'}
+                        mt={4}
+                        mb={4}
+                        colorScheme={'WhiteAlpha'}
+                    />
+                    <Wrap justify='center'>
+                    <Button
+                        size={'lg'}
+                        type="submit"
+                        disabled={status == "loading"}
+                        backgroundColor={'Green'}
+                    >Tilmeld
+                    </Button>
+                    </Wrap>
         
-        onSubmit={handleSubscribe}
-      >
+            </form>
 
-         <Input
-            type="text"
-            placeholder="Fornavn"
-            value={first_name}
-            onChange={(e) => setFirstName(e.target.value)}
-            disabled={status == "loading"}
-            size={'md'}
-          />
-        <Input
-            type="text"
-            placeholder="Efternavn"
-            value={last_name}
-            onChange={(e) => setLastName(e.target.value)}
-            disabled={status == "loading"}
-            size={'md'}
-          />
-        <Input
-            type="text"
-            placeholder="Telefon nummer"
-            value={phone_number}
-            onChange={(e) => setPhoneNumber(e.target.value)}
-            disabled={status == "loading"}
-            size={'md'}
-          />
-          <Input
-            type="email"
-            placeholder="E-mail"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            disabled={status == "loading"}
-            size={'md'}
-          />
-          
-            <Button
-                size={'md'}
-                type="submit"
-                disabled={status == "loading"}
-                backgroundColor={'WhiteSmoke'}
-            >Tilmeld
-            </Button>
-          
-        
-          
-        <div className="server-message pt-4 text-green-600">
-          {status === "success" ? (
-            <p className="text-green-600">{responseMsg}</p>
-          ) : null}
-          {status === "error" ? (
-            <p className="text-orange-600">{responseMsg}</p>
-          ) : null}
-        </div>
-      </form>
-    </Wrap>
+
+
                 
                   </ModalBody>
 
