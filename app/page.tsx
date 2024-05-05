84ffade65f5bd595745be442d805dc9f9d8b089d
T00few2 <christian.kjar@gmail.com>
2024-05-05 08:16:17 +0200

    heading size

--- app/page.tsx
+++ app/page.tsx
@@ -31,8 +31,8 @@ export default async function Page() {
     
     <Grid mt={8} alignContent={'center'} alignItems={'center'} justifyContent={'center'} alignSelf={'center'} justifySelf={'center'}>
       <Flex justifyContent={'center'} alignItems="center">
-      <Heading textAlign={'center'} mr={2} fontFamily={'Roboto'} fontWeight={900} fontSize={40}>Kjær Boligudlejning</Heading>
-      <Image boxSize='40px'  src='Logo.svg'/>
+      <Heading textAlign={'center'} mr={2} fontFamily={'Roboto'} fontWeight={900} fontSize={{base:30, sm:40, md:40}}>Kjær Boligudlejning</Heading>
+      <Image boxSize={{base:'30px', sm:'40px', md:'40px'}}  src='Logo.svg'/>
       </Flex>
       <Wrap justify={'center'} flexWrap={'wrap'} mt={4}>
         <Tooltip label='Ring 22 99 64 21'>
