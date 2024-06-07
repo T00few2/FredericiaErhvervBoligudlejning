d6aa7fc4b8c539125c45e296d877aba419449206
T00few2 <christian.kjar@gmail.com>
2024-06-07 15:20:43 +0200

    Cleanup + centering

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ import { IoCall } from "react-icons/io5";
 import { Metadata } from "next";
 import Footer from '../components/footer';
 
-import {Highlight, Popover,PopoverTrigger, PopoverCloseButton, PopoverBody, PopoverArrow, Portal, PopoverContent, Button, Box, Container, Flex, Heading, Stack, Text, Link, SimpleGrid, Wrap, Grid, Image, Tooltip, IconButton, HStack} from '@chakra-ui/react'
+import {Center, Highlight, Popover,PopoverTrigger, PopoverCloseButton, PopoverBody, PopoverArrow, Portal, PopoverContent, Button, Box, Container, Flex, Heading, Stack, Text, Link, SimpleGrid, Wrap, Grid, Image, Tooltip, IconButton, HStack} from '@chakra-ui/react'
 
 export const metadata: Metadata = {
   title: 'Fredericia Erhverv Boligudlejning',
@@ -69,164 +69,143 @@ export default async function Page() {
 
   return (
     
-    <Grid mt={8} alignContent={'center'} alignItems={'center'} justifyContent={'center'} alignSelf={'center'} justifySelf={'center'}>
+    <Container maxW={{base:'95vw', sm:'80vw', md:'70vw'}}>
       <Flex justifyContent={'center'}>
-      
-      <Image objectFit={'contain'} maxW={{base:'95vw', sm:'80vw', md:'60vw'}} mb={{base:1, sm:2, md:4}} src='logo.jpg'/>
+        <Image objectFit={'contain'} mt={8} maxW={{base:'95vw', sm:'80vw', md:'60vw'}} mb={{base:1, sm:2, md:4}} src='logo.jpg'/>
       </Flex>
       
       <Wrap spacingY={2} justify='center' mt={2}>
-        
-  
-  <Box textAlign="center" alignItems="center">
-    <Tooltip label='Ring 22 99 64 21'>
-      <Link href='tel:+4522996421' color='black' whiteSpace="pre-line">
-        <IconButton aria-label='Ring' color='rgb(27,52,90)' icon={<MdCall fontSize={30} />} />
-        <Text mt={2} fontWeight="bold" width={'100px'}>Ring</Text>
-      </Link>
-    </Tooltip>
-    </Box>
-  
-    <Box textAlign="center" alignItems="center">
-    <Tooltip label='SMS 22 99 64 21'>
-      <Link href='sms:+4522996421' color='black' whiteSpace="pre-line">
-        <IconButton aria-label='SMS' color='rgb(27,52,90)' icon={<FaSms fontSize={30}/>} />
-        <Text mt={2} fontWeight="bold" width={'100px'}>SMS</Text>
-      </Link>
-    </Tooltip>
-  </Box>
-  <Box textAlign="center" alignItems="center">
-    <Tooltip label='Email udlejning@fredericiabolig.com'>
-      <Link href='mailto:udlejning@fredericiabolig.com' color='black' whiteSpace="pre-line">
-        <IconButton aria-label='Mail' color='rgb(27,52,90)' icon={<MdEmail fontSize={30}/>} />
-        <Text mt={2} fontWeight="bold" width={'100px'}>E-mail</Text>
-      </Link>
-    </Tooltip>
-  </Box>
-  
-    <Newsletter />
-    <Box textAlign="center" alignItems="center">
-    <Tooltip label='Facebook'>
-      <Link href='https://www.facebook.com/share/Kg8pKAhod4BdmFP2/' color='black' whiteSpace="pre-line" target="_blank">
-        <IconButton aria-label='Mail' color='rgb(27,52,90)' icon={<FaFacebook fontSize={30}/>} />
-        <Text mt={2} fontWeight="bold" width={'100px'}>Facebook</Text>
-      </Link>
-    </Tooltip>
-  </Box>
-  
-</Wrap>
+        <Box textAlign="center" alignItems="center">
+          <Tooltip label='Ring 22 99 64 21'>
+            <Link href='tel:+4522996421' color='black' whiteSpace="pre-line">
+              <IconButton aria-label='Ring' color='rgb(27,52,90)' icon={<MdCall fontSize={30} />} />
+              <Text mt={2} fontWeight="bold" width={'100px'}>Ring</Text>
+            </Link>
+          </Tooltip>
+        </Box>
+        <Box textAlign="center" alignItems="center">
+          <Tooltip label='SMS 22 99 64 21'>
+            <Link href='sms:+4522996421' color='black' whiteSpace="pre-line">
+              <IconButton aria-label='SMS' color='rgb(27,52,90)' icon={<FaSms fontSize={30}/>} />
+              <Text mt={2} fontWeight="bold" width={'100px'}>SMS</Text>
+            </Link>
+          </Tooltip>
+        </Box>
+        <Box textAlign="center" alignItems="center">
+          <Tooltip label='Email udlejning@fredericiabolig.com'>
+            <Link href='mailto:udlejning@fredericiabolig.com' color='black' whiteSpace="pre-line">
+              <IconButton aria-label='Mail' color='rgb(27,52,90)' icon={<MdEmail fontSize={30}/>} />
+              <Text mt={2} fontWeight="bold" width={'100px'}>E-mail</Text>
+            </Link>
+          </Tooltip>
+        </Box>
+        <Newsletter />
+        <Box textAlign="center" alignItems="center">
+          <Tooltip label='Facebook'>
+            <Link href='https://www.facebook.com/share/Kg8pKAhod4BdmFP2/' color='black' whiteSpace="pre-line" target="_blank">
+              <IconButton aria-label='Mail' color='rgb(27,52,90)' icon={<FaFacebook fontSize={30}/>} />
+              <Text mt={2} fontWeight="bold" width={'100px'}>Facebook</Text>
+            </Link>
+          </Tooltip>
+        </Box>   
+      </Wrap>
+
 <Flex justifyContent="center" alignItems="center" mt={2}>
-
-  <Box maxW={{ base:'90vw', sm:'80vw', md:'60vw' }}>
-
-    <Wrap spacingY={2}>
+  <Box>
       <Text mt={4} whiteSpace="pre-line" textAlign={'center'} fontWeight="bold" lineHeight={2.2}>
-       
-          Velkommen til Fredericia Erhverv Boligudlejning – et 
+      Velkommen til Fredericia Erhverv Boligudlejning – et 
         <Popover>
-            <PopoverTrigger>
-              <Button ml='1' mr='1' verticalAlign={'center'} backgroundColor={'#CBD5E0'}  rounded='full' fontWeight={'bold'} px='2' py='1' size={'md'} height={'30px'} _hover={{ bg: 'rgb(27,52,90)', textColor: 'White' }} display={'inline-flex'}>familieejet boligselskab</Button>
-            </PopoverTrigger>
-            <Portal>
-              <PopoverContent borderWidth={0}>
-                <PopoverArrow />
-                <PopoverBody lineHeight={2} backgroundColor={'rgb(27,52,90)'} textColor={'White'} fontWeight={'bold'} rounded={'10'}>
-                <Highlight
-                  query={['ordentlig udlejer','velholdte lejligheder','fair priser','kontakt os for en fremvisning']}
-                  styles={{ px: '2', py: '1', rounded: 'full', bg: '#CBD5E0' }}>
-                  Vi stræber efter at være en ordentlig udlejer med velholdte lejligheder og en fair leje. Med en portefølje på mere end 80 lejligheder i Fredericia kan vi imødekomme de fleste behov og har løbende ledige lejligheder.
-                </Highlight>
-                 
-                </PopoverBody>
-              </PopoverContent>
-            </Portal>
-          </Popover>
+          <PopoverTrigger>
+            <Button ml='1' mr='1' verticalAlign={'center'} backgroundColor={'#CBD5E0'}  rounded='full' fontWeight={'bold'} px='2' py='1' size={'md'} height={'30px'} _hover={{ bg: 'rgb(27,52,90)', textColor: 'White' }} display={'inline-flex'}>familieejet boligselskab</Button>
+          </PopoverTrigger>
+          <Portal>
+            <PopoverContent borderWidth={0}>
+              <PopoverArrow />
+              <PopoverBody lineHeight={2} backgroundColor={'rgb(27,52,90)'} textColor={'White'} fontWeight={'bold'} rounded={'10'}>
+              <Highlight
+                query={['ordentlig udlejer','velholdte lejligheder','fair priser','kontakt os for en fremvisning']}
+                styles={{ px: '2', py: '1', rounded: 'full', bg: '#CBD5E0' }}>
+                Vi stræber efter at være en ordentlig udlejer med velholdte lejligheder og en fair leje. Med en portefølje på mere end 80 lejligheder i Fredericia kan vi imødekomme de fleste behov og har løbende ledige lejligheder.
+              </Highlight>  
+              </PopoverBody>
+            </PopoverContent>
+          </Portal>
+        </Popover>
           med rødder i Fredericia. Se hvad vi har ledigt nedenfor og 
-        
         <Popover>
-            <PopoverTrigger>
+          <PopoverTrigger>
             <Button ml='1' mr='1' verticalAlign={'center'} backgroundColor={'#CBD5E0'}  rounded='full' fontWeight={'bold'} px='2' py='1' size={'md'} height={'30px'} _hover={{ bg: 'rgb(27,52,90)', textColor: 'White' }} display={'inline-flex'}>kontakt os for en fremvisning</Button>
-            </PopoverTrigger>
-            <Portal>
-              <PopoverContent width="fit-content" minWidth="200px">
-                <PopoverArrow />
-                
-                <PopoverBody backgroundColor={'blue.50'} display='flex' alignItems='center' justifyContent='space-between'>
-                  <Wrap spacingY={2} justify='center' mt={2}>
-                  <Box textAlign="center" alignItems="center">
-                  <Tooltip label='Ring 22 99 64 21'>
-                    <Link href='tel:+4522996421' color='black' whiteSpace="pre-line">
-                      <IconButton aria-label='Ring' color='rgb(27,52,90)' icon={<MdCall fontSize={30} />} />
-                      <Text mt={2} fontWeight="bold" width={'100px'}>Ring</Text>
+          </PopoverTrigger>
+          <Portal>
+            <PopoverContent width="fit-content" minWidth="200px">
+              <PopoverArrow />
+              
+              <PopoverBody backgroundColor={'blue.50'} display='flex' alignItems='center' justifyContent='space-between'>
+                <Wrap spacingY={2} justify='center' mt={2}>
+                <Box textAlign="center" alignItems="center">
+                <Tooltip label='Ring 22 99 64 21'>
+                  <Link href='tel:+4522996421' color='black' whiteSpace="pre-line">
+                    <IconButton aria-label='Ring' color='rgb(27,52,90)' icon={<MdCall fontSize={30} />} />
+                    <Text mt={2} fontWeight="bold" width={'100px'}>Ring</Text>
+                  </Link>
+                </Tooltip>
+                </Box>
+                <Box textAlign="center" alignItems="center">
+                  <Tooltip label='SMS 22 99 64 21'>
+                    <Link href='sms:+4522996421' color='black' whiteSpace="pre-line">
+                      <IconButton aria-label='SMS' color='rgb(27,52,90)' icon={<FaSms fontSize={30}/>} />
+                      <Text mt={2} fontWeight="bold" width={'100px'}>SMS</Text>
                     </Link>
                   </Tooltip>
-                  </Box>
-                  <Box textAlign="center" alignItems="center">
-                    <Tooltip label='SMS 22 99 64 21'>
-                      <Link href='sms:+4522996421' color='black' whiteSpace="pre-line">
-                        <IconButton aria-label='SMS' color='rgb(27,52,90)' icon={<FaSms fontSize={30}/>} />
-                        <Text mt={2} fontWeight="bold" width={'100px'}>SMS</Text>
-                      </Link>
-                    </Tooltip>
-                  </Box>
-                  <Box textAlign="center" alignItems="center">
-                    <Tooltip label='Email udlejning@fredericiabolig.com'>
-                      <Link href='mailto:udlejning@fredericiabolig.com' color='black' whiteSpace="pre-line">
-                        <IconButton aria-label='Mail' color='rgb(27,52,90)' icon={<MdEmail fontSize={30}/>} />
-                        <Text mt={2} fontWeight="bold" width={'100px'}>E-mail</Text>
-                      </Link>
-                    </Tooltip>
-                  </Box>
-                  </Wrap>
-                </PopoverBody>
-              </PopoverContent>
-            </Portal>
-          </Popover>
-        
+                </Box>
+                <Box textAlign="center" alignItems="center">
+                  <Tooltip label='Email udlejning@fredericiabolig.com'>
+                    <Link href='mailto:udlejning@fredericiabolig.com' color='black' whiteSpace="pre-line">
+                      <IconButton aria-label='Mail' color='rgb(27,52,90)' icon={<MdEmail fontSize={30}/>} />
+                      <Text mt={2} fontWeight="bold" width={'100px'}>E-mail</Text>
+                    </Link>
+                  </Tooltip>
+                </Box>
+                </Wrap>
+              </PopoverBody>
+            </PopoverContent>
+          </Portal>
+        </Popover>
         <Highlight
           query={['tilmeld dig vores nyhedsmail']}
           styles={{ px: '2', py: '1', rounded: 'full', bg: 'blue.100' }}>
           Kan vi ikke opfylde dine ønsker lige nu, så
         </Highlight>
         <Popover>
-            <PopoverTrigger>
+          <PopoverTrigger>
             <Button ml='1' mr='1' verticalAlign={'center'} backgroundColor={'#CBD5E0'}  rounded='full' fontWeight={'bold'} px='2' py='1' size={'md'} height={'30px'} _hover={{ bg: 'rgb(27,52,90)', textColor: 'White' }} display={'inline-flex'}>tilmeld dig vores nyhedsmail eller Facebook side</Button>
-            </PopoverTrigger>
-            <Portal>
-              <PopoverContent width="fit-content">
-                <PopoverArrow />
-                
-                <PopoverBody backgroundColor={'blue.50'} display='flex' alignItems='center' justifyContent='space-between'>
-                  <Newsletter />
-                    <Box textAlign="center" alignItems="center">
-                    <Tooltip label='Facebook'>
-                      <Link href='https://www.facebook.com/share/Kg8pKAhod4BdmFP2/' color='black' whiteSpace="pre-line" target="_blank">
-                        <IconButton aria-label='Mail' color='rgb(27,52,90)' icon={<FaFacebook fontSize={30}/>} />
-                        <Text mt={2} fontWeight="bold" width={'100px'}>Facebook</Text>
-                      </Link>
-                    </Tooltip>
-                  </Box>
-                </PopoverBody>
-              </PopoverContent>
-            </Portal>
-          </Popover>
-          og få direkte besked, når der er nye ledige lejligheder.
+          </PopoverTrigger>
+          <Portal>
+            <PopoverContent width="fit-content">
+              <PopoverArrow />
+              
+              <PopoverBody backgroundColor={'blue.50'} display='flex' alignItems='center' justifyContent='space-between'>
+                <Newsletter />
+                  <Box textAlign="center" alignItems="center">
+                  <Tooltip label='Facebook'>
+                    <Link href='https://www.facebook.com/share/Kg8pKAhod4BdmFP2/' color='black' whiteSpace="pre-line" target="_blank">
+                      <IconButton aria-label='Mail' color='rgb(27,52,90)' icon={<FaFacebook fontSize={30}/>} />
+                      <Text mt={2} fontWeight="bold" width={'100px'}>Facebook</Text>
+                    </Link>
+                  </Tooltip>
+                </Box>
+              </PopoverBody>
+            </PopoverContent>
+          </Portal>
+        </Popover>
+        og få direkte besked, når der er nye ledige lejligheder.
         <br/><br/>
-        
-          Velkommen hjem – hos Fredericia Erhverv Boligudlejning!
-        
+        Velkommen hjem – hos Fredericia Erhverv Boligudlejning!
       </Text>
-    </Wrap>
-
   </Box>
-
 </Flex>
-        <Stack  spacing={4} as={Container} maxW={'5xl'} mt={12} mb={20} align={'center'}>
-        <SimpleGrid spacing={8} minChildWidth='290px'>
-          <Thumbnails lejligheder={lejligheder} ejendomme={ejendomme}/>
-        </SimpleGrid>
-      </Stack>
-      <Footer/>
-    </Grid>
+  <Thumbnails lejligheder={lejligheder} ejendomme={ejendomme}/>
+  <Footer/>
+</Container>    
   );
 }
--- components/PropertyCard.tsx
+++ components/PropertyCard.tsx
@@ -2,8 +2,6 @@
 'use client'
 import React from 'react';
 import { Ejendom } from '../queries/ejendom';
-import { CardFooter, Image } from '@chakra-ui/react';
-import { useDisclosure } from '@chakra-ui/react';
 import Carousel from './carusel';
 import './css/ribbonProperty.css'
 
@@ -12,28 +10,16 @@ import {
   ModalOverlay,
   ModalContent,
   ModalHeader,
-  ModalFooter,
   ModalBody,
   ModalCloseButton,
-  Button,
-  Flex,
   Heading,
   Text,
   Card,
   CardBody,
-  Stack,
-  Box,
-  Wrap,
-  
+  Image,
+  useDisclosure,
+  AspectRatio,
 } from '@chakra-ui/react'
-import { AspectRatio } from '@chakra-ui/react'
-
-import { PiLayout } from "react-icons/pi";
-import { SlSizeFullscreen } from "react-icons/sl";
-import { IoPricetagOutline } from "react-icons/io5";
-import { CiVault } from "react-icons/ci";
-import { LiaFileInvoiceDollarSolid } from "react-icons/lia";
-import { MdOutlineEventAvailable } from "react-icons/md";
 
 // Ribbon component
 const Ribbon = ({ text }) => {
@@ -53,8 +39,6 @@ const PropertyCard: React.FC<PropertyProps>=({ejendom}) => {
   const encodedLatitude = encodeURIComponent(ejendom.beliggenhed.latitude);
   const encodedLongitude = encodeURIComponent(ejendom.beliggenhed.longitude);
   
-  
-
   return (
     
       <Card onClick = {onOpen} variant={'elevated'} borderWidth={{ base:'2px', sm: '3px', md:'4px'}} borderRadius="lg" backgroundColor='#EDF2F7' borderColor={'#E2E8F0'} _hover={{ cursor: 'pointer' }}> 
@@ -62,43 +46,31 @@ const PropertyCard: React.FC<PropertyProps>=({ejendom}) => {
             <Ribbon text="Ingen ledige lejligheder" />
         )}
       <CardBody>
-        
           <Image objectFit='cover' aspectRatio={4/3} src = {ejendom.billeder[0]?.url} style={{ width: '100%', height: 'auto' }} />
-        
           <Heading size={['sm','sm','md']} mt={4}>{ejendom.navn}</Heading>
       </CardBody>
-          <CardFooter justify={'space-between'} flexWrap={'wrap'}>
-              
-          </CardFooter>
-
           <Modal isOpen={isOpen} onClose={onClose} motionPreset='scale' size ={{ base:'sm', sm: 'xl', md:'3xl'}} scrollBehavior={'inside'}>
-              
-              <ModalOverlay       
-              bg='blackAlpha.700'
-              backdropFilter='auto'
-              backdropInvert='20%'
-              backdropBlur='2px'
-              />
-              <ModalContent backgroundColor={'GhostWhite'} justifyContent={'center'}>
+            <ModalOverlay       
+            bg='blackAlpha.700'
+            backdropFilter='auto'
+            backdropInvert='20%'
+            backdropBlur='2px'
+            />
+            <ModalContent backgroundColor={'GhostWhite'} justifyContent={'center'}>
               <ModalHeader fontSize={'3xl'} fontWeight={'700'}>{ejendom.navn}</ModalHeader>
               <ModalCloseButton />
-              <ModalBody>
-              <Carousel
-                      cards={ejendom.billeder.map((billede) => billede.url)}
-                  />
-                  
+                <ModalBody>
+                  <Carousel cards={ejendom.billeder.map((billede) => billede.url)}/>
                   <Heading size ='lg' mt={4}>Beskrivelse</Heading>
                   <Text mt={4}>{ejendom.beskrivelse}</Text>
                   <Heading size ='lg' mt={4}>Kort</Heading>
                   <AspectRatio ratio={16 / 9} mt={4} mb={4}>
                   <iframe src={`https://www.google.com/maps?q=${encodedLatitude},${encodedLongitude}&z=15&output=embed`}></iframe>
                   </AspectRatio>
-                  </ModalBody>
-
-              </ModalContent>
+                </ModalBody>
+            </ModalContent>
           </Modal>
       </Card>
   );
 }
-
 export default PropertyCard;
